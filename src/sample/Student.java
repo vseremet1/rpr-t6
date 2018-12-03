f3db0ef350486f3b5ec99c379da1d7671aee55e1
vseremet1 <vseremet1@etf.unsa.ba>
2018-12-03 10:28:30 +0100

    klasa student

--- src/sample/Student.java
+++ src/sample/Student.java
@@ -3,19 +3,26 @@ package sample;
 public class Student {
 
     String ime,prezime,datumRodjenja,brojTelefona,adresa,mjestoRodjenja;
-    int brojIndeksa,JMBG;
+    String brojIndeksa,JMBG,Email,odsjek,status,borci,ciklus,godina;
 
 
-    public Student(String ime, String prezime, int brojIndeksa, String adresa, String telefon, int JMBG, String datum, String mjesto) {
 
-        this.ime=ime;
-        this.prezime=prezime;
-        this.brojIndeksa=brojIndeksa;
-        this.adresa=adresa;
-        this.brojTelefona=telefon;
-        this.JMBG=JMBG;
+    public Student(String ime, String prezime, String brojIndeksa, String mjesto, String jmbg, String adresa,
+                   String telefon, String email, String datum, String odsjek1, String borci1, String status1, String godina1, String ciklus1) {
+
+        this.ime = ime;
+        this.prezime= prezime;
         this.datumRodjenja=datum;
+        this.brojTelefona=telefon;
+        this.adresa=adresa;
         this.mjestoRodjenja=mjesto;
-
+        this.brojIndeksa=brojIndeksa;
+        this.JMBG=jmbg;
+        this.Email=email;
+        this.odsjek=odsjek1;
+        this.status = status1;
+        this.borci=borci1;
+        this.ciklus=ciklus1;
+        this.godina=godina1;
     }
 }
